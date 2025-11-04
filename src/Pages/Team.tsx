eba94f627fa8ce4c5f8e8b893cfd3eb9c8903c3e
Zeus Christian <aggabaozeuschristian@gmail.com>
2025-11-04 22:04:40 +0800

    Updated About Us. Span coloring, Major changes in Projects. and Projects changes into Creations

--- src/Pages/Team.tsx
+++ src/Pages/Team.tsx
@@ -41,7 +41,7 @@ const Team = () => {
   return (
     <section id="team" className="bg-[#030406] text-white py-24">
             <div className="container mx-auto px-6 text-center align-center">
-        <h2 className="About-h2 text-5xl font-extrabold mb-4">MEET THE TEAM</h2>
+        <h2 className="About-h2 text-5xl font-extrabold mb-4">MEET THE <span>TEAM</span></h2>
         <p className="About-p text-gray-300 mx-auto mb-12 align-center ">
           Our team is a group of passionate developers, designers, and thinkers who love turning challenges into solutions.
         </p>
